ba82f7ca6b24d8135e6c60ac452575f158c22279
sadmanahmed <abirsadmanahmed177@gmail.com>
2020-08-16 05:48:23 +0600

    Post Display

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,20 +1,41 @@
 class PicsController < ApplicationController
+before_action :find_pic, only: [:show,:edit,:update,:destroy]
 
 def index
 
 end
 
+def show
+
+end
 def new
   @pic = Pic.new
 end
 
 def create
   @pic =Pic.new(pic_params)
+
+  if @pic.save
+    redirect_to @pic,notice: "Yesss! It is posted"
+  else
+    render 'new'
+  end
+
+  def destroy
+
+  end
+  def update
+
+  end
 end
 
 
-Private
+private
   def pic_params
     params.require(:pic).permit(:title,:description)
+  end
+
+  def find_pic
+    @pic = Pic.find(params[:id])
   end
 end
